662b3cfcd78bd062f83d8eecd2a1a9f6671a14af
Arturo Rivera <arturo.riverar97@gmail.com>
2022-04-29 15:22:24 -0500

    Extract information from form
    The variables containing the corresponding inputs were declared as
    global to manipulate them outside the scope of the function that
    extracts the information.
    
    The element of the form was given an event listener "submit" that calls
    the function collectDataForm that updates the values of the inputs and
    also prevents the submit button from refreshing the page.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,9 @@
 const containerForm = document.querySelector('.popup_form');
+let author = document.getElementById('author').value;
+let title = document.getElementById('title').value;
+let pagesNumber = document.getElementById('pages').value;
+
+/*---------- Funciones --------*/
 
 function openForm(e) {
     containerForm.style.display = 'flex';
@@ -8,8 +13,24 @@ function closeForm(e) {
     containerForm.style.display = 'none';
 }
 
+function collectDataForm(e) {
+    e.preventDefault();
+
+    /*---------- Select inputs --------- */
+    author = document.getElementById('author').value;
+    title = document.getElementById('title').value;
+    pagesNumber = document.getElementById('pages').value;
+    console.log(author);
+    console.log(title);
+    console.log(pagesNumber);
+}
+
+
 const addNewBook = document.querySelector('.add_book');
 addNewBook.addEventListener('click', openForm);
 
 const closeButtonForm = document.querySelector('.close');
 closeButtonForm.addEventListener('click', closeForm);
+
+const form = document.getElementById('form');
+form.addEventListener('submit', collectDataForm);
